2078face350c936df4277845e4bf5dc2de488e9f
pkukreti <pkukreti@egnyte.com>
2024-10-15 22:42:51 +0530

    chore: updated navbar item and resume css

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
+import "./ResumeNew.css";
 import { HorizontalRule } from "@mui/icons-material";
 import pdf from "../../Assets/pushpendra.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
@@ -40,18 +41,6 @@ function ResumeNew() {
           <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
-        </Row>
-
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
         </Row>
       </Container>
     </div>
